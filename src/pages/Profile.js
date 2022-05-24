c5430a9f21d670a6506c11e755ad679a006fe4f2
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2022-05-24 18:45:30 +0100

    Favourites data ready to display

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -10,56 +10,95 @@ function Profile() {
   const [profileName, setProfileName] = useState("");
   const [titleIds, setTitleIds] = useState([]);
   const [state, dispatch] = useContext(Context);
-  const [image, setImage] = useState(null);
-  
-  function getImage(data) {
-    setImage(data.backdrop)
-    }
-  
-  //get request to API end point in the backend
-  useEffect(() => {
-    Axios.get(`http://localhost:3000/profile/${state.id}`)
-    .then((response) => {
-      // console.log("Data: " + response.data);
-      const data = response.data;
-      handleResponse(data)
-    .then(getImage (data) {
-      // console.log(data);
-      getImage(data);;
-    });
-  }, [])};
+  const [titleInfo, setTitleInfo] = useState({
+    one: {
+      title: "",
+      id: "",
+      poster: "/",
+      watchLink: "/",
+      imdbId: "",
+    },
+    two: {
+      title: "",
+      id: "",
+      poster: "/",
+      watchLink: "/",
+      imdbId: "",
+    },
+    three: {
+      title: "",
+      id: "id",
+      poster: "/",
+      watchLink: "/",
+      imdbId: "",
+    },
+    four: {
+      title: "",
+      id: "",
+      poster: "/",
+      watchLink: "/",
+      imdbId: "",
+    },
+  });
 
-  // Use the data from the GET api response
+  //get request to API end point in the backend, send data to handleResponse func
+  useEffect(() => {
+    Axios.get(`http://localhost:3000/profile/${state.id}`).then((response) => {
+      const data = response.data;
+      handleResponse(data);
+    });
+  }, []);
+
+  // Add data from GET api to array
   function handleResponse(data) {
     setTitleIds(
       data.map((row) => {
         return row.Title_id;
       })
-    
     );
-    }
-  )
-
-    
-    // console.log("Title Ids: " + titleIds);
+    console.log(titleIds);
     apiCall();
   }
 
-  // function APIcall() {
-  //   const apiKey = NuY1wbXmkOwxSZp6anpgcJR6oCmxJ06tTrDwJpNN;
-  //   titleimage.map(Axios.get(`https://api.watchmode.com/v1/title/${state.id}/details/?apiKey=${apiKey}&append_to_response=sources`)
-  //   )
-  //   .then((response) => {
-  //     console.log(response)
-  //   })
-  // }
+  // Set states
+  function updateStates(dataArray) {
+    setTitleInfo({
+      one: {
+        title: dataArray[0].title,
+        id: dataArray[0].id,
+        poster: dataArray[0].poster,
+        watchLink: dataArray[0].sources[0].web_url,
+        imdbId: dataArray[0].imdb_id,
+      },
+      two: {
+        title: dataArray[1].title,
+        id: dataArray[1].id,
+        poster: dataArray[1].poster,
+        watchLink: dataArray[1].sources[0].web_url,
+        imdbId: dataArray[1].imdb_id,
+      },
+      three: {
+        title: dataArray[2].title,
+        id: dataArray[2].id,
+        poster: dataArray[2].poster,
+        watchLink: dataArray[2].sources[0].web_url,
+        imdbId: dataArray[2].imdb_id,
+      },
+      four: {
+        title: dataArray[3].title,
+        id: dataArray[3].id,
+        poster: dataArray[3].poster,
+        watchLink: dataArray[3].sources[0].web_url,
+        imdbId: dataArray[3].imdb_id,
+      },
+    });
+  }
 
   function apiCall() {
     const dataArray = [];
     titleIds.map((id) => {
       const apiKey = "NuY1wbXmkOwxSZp6anpgcJR6oCmxJ06tTrDwJpNN";
       const apiUrl = `https://api.watchmode.com/v1/title/${id}/details/?apiKey=${apiKey}&append_to_response=sources`;
-
       fetch(apiUrl)
         .then((response) => {
           return response.json();
@@ -67,12 +106,14 @@ function Profile() {
         .then((json) => {
           let data = json;
           dataArray.push(data);
+          return dataArray;
+        })
+        .then(function (dataArray) {
+          // Call the updateStates function with the dataArray as an argument
+          updateStates(dataArray);
         })
         .catch((err) => console.error(err));
     });
-
-    // Array with info of all ids
-    console.log(dataArray);
   }
 
   return (
@@ -87,6 +128,8 @@ function Profile() {
                 <div className="Name">
                   <h3>{state.name}'s Favourites</h3>
                   <h3>{titleIds}</h3>
+                  <p>{titleInfo.one.title}</p>
+                  <p>{titleInfo.two.title}</p>
                 </div>
               </div>
             </h3>
@@ -97,6 +140,6 @@ function Profile() {
       </div>
     </div>
   );
-};
+}
 
-export default Profile
+export default Profile;
